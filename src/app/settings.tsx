fab29ae2da501411135fb2c37db4b4640216efa1
Anton Ladan <ladananton450@outlook.com>
2025-05-17 19:57:52 +0200

    settings wip

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,7 +1,29 @@
-import { View } from "react-native";
+import Storage from "expo-sqlite/kv-store";
+import { useEffect, useState } from "react";
+import { Button, View } from "react-native";
 import MySlider from "../components/slider/Slider";
 
 export default function Settings() {
+  let [savedWarmth, setSavedWarmth] = useState(0);
+  let [savedContrast, setSavedContrast] = useState(0);
+
+  try {
+    let warmth = Storage.getItemSync("warmth");
+    if (warmth) setSavedWarmth(parseInt(warmth));
+  } catch {}
+  try {
+    let contrast = Storage.getItemSync("contrast");
+    if (contrast) setSavedContrast(parseInt(contrast));
+  } catch {}
+
+  let [warmth, setWarmth] = useState(savedWarmth);
+  let [contrast, setContrast] = useState(savedContrast);
+
+  useEffect(() => {
+    console.log(`Varmoća je sad ${warmth} bičiz ;P`);
+    console.log(`Kontrast je sad ${contrast} bičiz ;P`);
+  }, [warmth, contrast]);
+
   return (
     <View
       style={{
@@ -14,7 +36,17 @@ export default function Settings() {
         text={"Warmth"}
         iconMin={"snow-outline"}
         iconMax={"sunny-outline"}
+        value={warmth}
+        setValue={setWarmth}
       />
+      <MySlider
+        text={"Warmth"}
+        iconMin={"ellipse-outline"}
+        iconMax={"contrast-outline"}
+        value={contrast}
+        setValue={setContrast}
+      />
+      <Button title="Save changes" />
     </View>
   );
 }
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -6,23 +6,35 @@ interface MySliderProps {
   text: string;
   iconMin: string;
   iconMax: string;
+  value: number;
+  setValue: any; // I hate this but okay
 }
 
-export default function MySlider({ text, iconMin, iconMax }: MySliderProps) {
+export default function MySlider({
+  text,
+  iconMin,
+  iconMax,
+  value,
+  setValue,
+}: MySliderProps) {
   return (
     <View style={styles.mySlider}>
-      <Text>Warmth</Text>
+      <Text>{text}</Text>
       <View style={styles.view}>
         <Ionicons name={iconMin} size={24} color="black" />
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={100}
-          minimumTrackTintColor="#1FB28A"
-          maximumTrackTintColor="#d3d3d3"
-          thumbTintColor="#1FB28A"
-        />
-        <Ionicons name={iconMax} size={24} color="black" />
+        <View style={styles.sliderContainer}>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={100}
+            minimumTrackTintColor="#1FB28A"
+            maximumTrackTintColor="#d3d3d3"
+            thumbTintColor="#1FB28A"
+            value={value}
+            onValueChange={setValue}
+          />
+          <Ionicons name={iconMax} size={24} color="black" />
+        </View>
       </View>
     </View>
   );
@@ -31,15 +43,22 @@ export default function MySlider({ text, iconMin, iconMax }: MySliderProps) {
 let styles = StyleSheet.create({
   mySlider: {
     flexDirection: "column",
+    width: "100%",
   },
   view: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 20,
+    justifyContent: "space-between", // make it breathe
   },
   icon: {
     width: 20,
     height: 20,
+  },
+  sliderContainer: {
+    flex: 1,
+    marginHorizontal: 10,
   },
   slider: {
     flex: 1,
